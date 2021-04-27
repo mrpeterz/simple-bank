88db133bf85a4123920c19aacc99412cd66f07fd
Pietro Pagani <pietro.pagani.backup@gmail.com>
2021-04-28 00:34:18 +0200

    Add users migrations

--- migrations/Version20210427140229.php
+++ migrations/Version20210427140229.php
@@ -7,25 +7,27 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20210427140229 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'create_customers_table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+            CREATE TABLE users (
+                id VARCHAR(255) NOT NULL, 
+                name VARCHAR(50) NOT NULL, 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ');
 
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('DROP TABLE users');
     }
 }
